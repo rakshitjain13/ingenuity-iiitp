671c5726acbeead5109721d2e4117094fcd6e729
Rakshit Jain <rakshitdhoka13@gmail.com>
2020-07-01 22:48:19 +0530

    Slider Update

---
+++ src/component/HomeComponent.js
@@ -0,0 +1,50 @@
+import React,{Component } from 'react';
+import {intern} from '../shared/source';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
+import '../slider-animations.css';
+import './styles.css';
+
+
+class Home extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            exprience:intern
+        }
+    }
+    render(){
+        const content=this.state.exprience.filter(item=>item.slider);
+        console.log(content);
+        return(
+            <div className="container">
+                <div className="row justify-content-center" style={{margin:10}}>
+                    <div className="col-12 col-md-10 ">
+                    <Slider className="slider-wrapper" autoplay={3000}>
+			            {content.map((item, index) => (
+			        	<div
+					key={index}
+					className="slider-content"
+					style={{ background: `url('${item.image}') no-repeat center center` }}
+			    	>
+					<div className="inner">
+						<h1>{item.title}</h1>
+						<p>{item.description}</p>
+						<button>{item.button}</button>
+					</div>
+					<section>
+						<img src={item.userProfile} alt={item.user} />
+						<span>
+							Posted by <strong>{item.user}</strong>
+						</span>
+					</section>
+				</div>
+			))}
+		</Slider>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default Home;
--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -1,5 +1,7 @@
 import React,{Component } from 'react';
 import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 class Main extends Component{
     constructor(props){
@@ -8,11 +10,10 @@ class Main extends Component{
     render(){
         return(
             <div>
-                <Header/>
-                <Switch key={this.props.location}>
-                    <Route path='home'>
-                    <Redirect to="/home" />
-                    </Route>
+            <Header/>
+                <Switch location={this.props.location}>
+                    <Route path='/home' component={()=><Home/>} />
+                    <Redirect to='/home' />
                 </Switch>
             <Footer/>
             </div>
