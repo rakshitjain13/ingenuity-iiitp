f8362d5029191018af06b472b094a57b1899df1c
Mukhrit <mukhritgupta20@gmail.com>
2020-07-23 23:14:42 +0530

    css issues updated

--- src/component/ViewComponent.js
+++ src/component/ViewComponent.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-// import '../css/ViewComponent.css';
+import '../css/ViewComponent.css';
 import { data } from '../shared/source';
 import { Link } from 'react-router-dom';
-
 
 class View extends Component {
   constructor(props) {
@@ -34,16 +33,21 @@ class View extends Component {
 
             <div class='col-md-4 cover-details'>
               <div class='mt-4'>
-              <h1>
-                <Link
-                  to={`${firstitem.type}/${firstitem.id}`}
-                  className='cover-link'
-                >
-                  {firstitem.title}
-                </Link>
-                </h1>
-            </div>
-              <div class='dt'>{firstitem.date} <span style={{color:'blue'}}>|</span> {firstitem.user}</div>
+                <div className='a-parent'>
+                  <h1>
+                    <Link
+                      to={`${firstitem.type}/${firstitem.id}`}
+                      className='cover-link'
+                    >
+                      {firstitem.title}
+                    </Link>
+                  </h1>
+                </div>
+              </div>
+              <div class='dt'>
+                {firstitem.date} <span style={{ color: 'blue' }}>|</span>{' '}
+                {firstitem.user}
+              </div>
               <div class='dt'>Reading Time : {firstitem.time}</div>
               <div class='cover-excerpt'>{firstitem.description}</div>
             </div>
@@ -66,9 +70,15 @@ class View extends Component {
                 <div className='col-12 col-md-5 mb-5'>
                   <div class='media-body'>
                     <h3 class='title mb-1 ml-0'>
-                      <Link to={`${item.type}/${item.id}`}>{item.title}</Link>
+                      <div className='a-parent'>
+                        <Link to={`${item.type}/${item.id}`}>{item.title}</Link>
+                      </div>
                     </h3>
-                    <div className='dt ml-1'> {item.date} <span style={{color:'blue'}}>|</span> {item.user}</div>
+                    <div className='dt ml-1'>
+                      {' '}
+                      {item.date} <span style={{ color: 'blue' }}>|</span>{' '}
+                      {item.user}
+                    </div>
                     <div class='dt ml-1'>Reading Time : {item.time}</div>
                     <div class='intro ml-1'>{item.description}</div>
                     {/* <a class='more-link' href='pages/blog-post.php'>
@@ -78,20 +88,6 @@ class View extends Component {
                 </div>
               </div>
             ))}
-            {/* <nav class='blog-nav nav nav-justified my-5'>
-              <Link
-                class='nav-link-prev nav-item nav-link d-none rounded-left'
-                href='https://www.free-css.com/free-css-templates'
-              >
-                Previous<i class='arrow-prev fas fa-long-arrow-alt-left'></i>
-              </Link>{' '}
-              <Link
-                class='nav-link-next nav-item nav-link rounded'
-                href='pages/blog-list.php'
-              >
-                Next<i class='arrow-next fas fa-long-arrow-alt-right'></i>
-              </Link>
-            </nav> */}
           </div>
         </section>
       </div>
