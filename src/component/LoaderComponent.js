38119f17a4bd8550e5e1251f2851aa83ff54d8f7
Mukhrit <mukhritgupta20@gmail.com>
2020-08-31 18:42:27 +0530

    test

--- src/component/LoaderComponent.js
+++ src/component/LoaderComponent.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import Loader from 'react-loader-spinner';
+// import React from 'react';
+// import Loader from 'react-loader-spinner';
 
-function Load(props) {
+// function Load(props) {
+//   return (
+//     <div
+//       className='justify-content-center'
+//       style={{ marginLeft: '47%', justifyContent: 'center' }}
+//     >
+//       <Loader type='ThreeDots' color='#000' height={60} width={60} />
+//     </div>
+//   );
+
+// export default Load;
+import React from 'react';
+import '../css/Loader.css';
+
+function Load() {
   return (
-    <div
-      className='justify-content-center'
-      style={{ marginLeft: '47%', justifyContent: 'center' }}
-    >
-      <Loader type='ThreeDots' color='#000' height={60} width={60} />
+    <div>
+      <div id='preloader'>
+        <div id='loader'></div>
+      </div>
     </div>
   );
 }
